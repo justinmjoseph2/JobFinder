000cf10ccd8ce7c633caf6b6147662ef3e3199e2
Justin M Joseph <justinmjoseph2.com>
2024-09-22 20:11:22 +0530

    Prepare for deployment on Render

--- jobPlatform/views.py
+++ jobPlatform/views.py
@@ -2088,13 +2088,12 @@ def provider_registration_chart(request):
 
 
 
-# View function to handle resume uploads and analysis
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import ResumeUpload
 import os
 
-@login_required_custom
+@login_required
 def analyze_resume(request):
     if request.method == 'POST' and request.FILES.get('resume'):
         # Handle file upload
@@ -2166,7 +2165,7 @@ def analyze_resume(request):
         )
 
         # Send messages to get responses
-        response1 = chat_session.send_message("Analyze the resume for suitable job matches. when listing, provide details such as job title, average salary a person could get in INR and why it is suitable. dont provide any other data. show job title at the beggining. avoid heading like suitable jobs too. use you/your for pointing to the person and display the best job that suets the user. also include ':' after the heading.")
+        response1 = chat_session.send_message("Analyze the resume for suitable job matches. when listing, provide details such as job title, average salary a person could get in INR and why it is suitable. dont provide any other data. show job title at the beginning. avoid heading like suitable jobs too. use you/your for pointing to the person and display the best job that suits the user. also include ':' after the heading.")
         response3 = chat_session.send_message("Analyze the resume and suggest points to improve the quality of the resume and ATS score. dont provide any other data. avoid heading like resume improvements too. use you/your for pointing to the person")
 
         suitable_jobs = response1.text.replace('#', '').replace('*', '')
